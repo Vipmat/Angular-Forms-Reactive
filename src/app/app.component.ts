e7f6d519b152ea5301d279c4ca01f76396ce31b2
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-09-21 10:40:09 +0100

    Reacting to status or value changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -18,10 +19,22 @@ export class AppComponent implements OnInit {
           Validators.required,
           this.forbiddenNames.bind(this),
         ]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([]),
+    });
+
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
+
+    this.signupForm.statusChanges.subscribe((status) => {
+      console.log(status);
     });
   }
 
@@ -43,5 +56,18 @@ export class AppComponent implements OnInit {
       return { nameIsForbidden: true };
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
